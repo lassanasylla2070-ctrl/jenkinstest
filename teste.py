08bc9e7bd17bd02661c59dfe82f23709e3233942
wafaroukhmi-tech <wafa.roukhmi@gmail.com>
2026-01-30 13:18:21 +0100

    Convert script to Flask web application

--- teste.py
+++ teste.py
@@ -1,1 +1,10 @@
-print(Salut ! Ma mise à jour automatique via Jenkins marche parfaitement ! ✅")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Salut ! Ma mise à jour automatique via Jenkins marche parfaitement ! ✅'
+
+if __name__ == '__main__':
+    # On écoute sur 0.0.0.0 pour que ce soit accessible hors du conteneur
+    app.run(host='0.0.0.0', port=5000)
